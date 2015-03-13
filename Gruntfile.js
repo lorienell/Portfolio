2965bd801a19fcdabe4c6ef19a7bfd48a852afdd
lorienell <bagmet_lesya@inbox.ru>
2015-03-13 20:35:49 +0200

    grunt installed

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+﻿module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    encoding: {
+      options: {
+        encoding: 'UTF8'
+      },
+      files: {
+        src: ['dist/*html']
+      }
+    },
+
+    bake: {
+      build: {
+        options: {
+          content: "locales/en_us.json",
+          section: "en"
+        },
+        files: {
+          "dist/index.html": "pages/index.html"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bake');
+  grunt.registerTask('default', ['bake']);
+};
